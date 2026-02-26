4708d148daba491c4577fe11025149982fad4c37
Gerardo Herrera <gerardo.herrera@unl.edu.ec>
2026-02-25 19:20:03 -0500

    añadir producto modelo

---
+++ backend/src/main/java/com/unlLoleros/backend/modelos/Producto.java
@@ -0,0 +1,16 @@
+package com.unlLoleros.backend.modelos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Producto")
+
+public class Producto {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private double precioProducto;
+    @Enumerated(EnumType.STRING)
+    private Unidad unidad; 
+}
