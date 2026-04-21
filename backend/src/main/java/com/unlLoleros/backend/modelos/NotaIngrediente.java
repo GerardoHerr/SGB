241cbe4ece82cbf7d8bb2b738970c09cdcb4c938
IsaKaiser04 <isauro.rivera@unl.edu.ec>
2026-04-20 23:04:54 -0500

    Nota Ingrediente

--- backend/src/main/java/com/unlLoleros/backend/modelos/NotaIngrediente.java
+++ backend/src/main/java/com/unlLoleros/backend/modelos/NotaIngrediente.java
@@ -1,7 +1,12 @@
 package com.unlLoleros.backend.modelos;
 
-import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "NotaIngrediente")
